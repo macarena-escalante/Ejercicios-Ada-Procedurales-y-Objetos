1fdd9ca28751b14a9d0003451ba6ee6fcae952a6
Maca <53454834+macarena-escalante@users.noreply.github.com>
2019-08-15 09:30:03 -0300

    Temporada completa de TWD

---
+++ Netflix/src/app/Temporada.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Temporada
+ */
+public class Temporada {
+
+    public int nroTemporada;
+    public List<Episodio> episodios = new ArrayList<Episodio>();
+    public String nombre;
+    public int anio;
+
+    public Episodio getNumeroEpisodio(int numero) {
+        for (Episodio e : this.episodios) {
+            if (numero == e.nroEpisodio)
+                return e;
+
+        }
+        return null;
+    }
+
+    public Episodio getNombreEpisodio(String nombre) {
+        for (Episodio n : this.episodios) {
+            if (nombre.equals(n.titulo)) {
+                return n;
+
+            }
+        }
+        return null;
+    }
+    public Episodio getEpisodio(int numero) {
+
+        for (Episodio e : this.episodios) {
+            if (numero == e.nroEpisodio)
+                return e;
+
+        }
+        return null;
+
+    }
+
+}
